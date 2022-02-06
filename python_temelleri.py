beed09a4d115e402a1a68f3c4af1c99cc2e63d0a
Fatma Nur <fatmanurtml@gmail.com>
2022-02-07 01:16:49 +0300

    Update python_temelleri.py

--- python_temelleri.py
+++ python_temelleri.py
@@ -5,4 +5,22 @@ print("Hello Python")
 
 #Karşıdan veri almayı sağlayan fonksiyon.
 input("İsminiz nedir?")
-#imput fonksiyonu kullanıcıdan bir girdi alır ve bu girdiği ekrana yazdırır.
+#imput fonksiyonu kullanıcıdan bir girdi alır ve bu girdiyi ekrana yazdırır.
+
+
+### Değişkenler ve Sabitler (Variables and Constants)
+
+#değişkenler küçük harf ile tanımlanır
+isim = "Fatmanur"
+soyisim = "Temel"
+print(isim+soyisim)
+
+#sabitler büyük harf ile tanımlanır
+PI = 3.14
+
+#değişkenler yan yana da atanabilir
+isim,soyisim = "Ali","Tek"
+
+#değişknelerin yer değiştirmesi tek satırda yapılabilir.
+isim,soyisim = soyisim,isim
+print(isim)
