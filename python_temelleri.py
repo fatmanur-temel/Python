07e157627eb941d6cf5cee1b72b7c21530f6ab4d
Fatma Nur <fatmanurtml@gmail.com>
2022-02-07 00:50:52 +0300

    Create python_temelleri.py

---
+++ python_temelleri.py
@@ -0,0 +1,8 @@
+# Python Programlama Dili Temelleri
+
+#Ekrana çıktı veren fonksiyon.
+print("Hello Python")
+
+#Karşıdan veri almayı sağlayan fonksiyon.
+input("İsminiz nedir?")
+#imput fonksiyonu kullanıcıdan bir girdi alır ve bu girdiği ekrana yazdırır.
